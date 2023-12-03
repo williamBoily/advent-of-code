57ea303d867b8bd2b198cd8234edc51f597671f9
William Boily <william.boily@hotmail.ca>
2023-12-02 20:27:02 -0500

    day 02

---
+++ 2023/day02/part_2.php
@@ -0,0 +1,49 @@
+<?php
+namespace AOC2023;
+
+require_once __DIR__ . '/Script.php';
+
+$script = new Script();
+$script->printInputFile();
+
+$inputs = file($script->inputFilePath, FILE_IGNORE_NEW_LINES);
+if(false === $inputs){
+	echo 'Cannot read input file';
+	exit;
+}
+
+$nano_start = hrtime(true);
+
+$total = 0;
+foreach ($inputs as $key => $line) {
+	$gameId = preg_replace('/\D/', '', substr($line, 0, strpos($line, ': ')));
+	$sets = substr($line, strpos($line, ': ') + 2);
+	$sets = explode(';', $sets);
+
+	$gameColors = [];
+	foreach ($sets as $key => $cubes) {
+		$cubes = explode(', ', $cubes);
+		
+		foreach ($cubes as $colorDraw) {
+			list($value, $color) = sscanf($colorDraw, '%d %s');
+			if(!isset($gameColors[$color])){
+				$gameColors[$color] = [];
+			}
+			$gameColors[$color][] = (int)$value;
+		}
+	}
+	$power = 1;
+	foreach ($gameColors as $colorName => $values) {
+		$power *= max($values);
+	}
+
+	$total += $power;
+}
+
+echo "$total\n";
+
+$nano_end = hrtime(true);
+$nano_time = $nano_end - $nano_start;
+$time = $script->nanoToSec($nano_time);
+echo "sec:$time\n";
+exit;
